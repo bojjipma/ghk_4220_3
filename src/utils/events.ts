000bb26b6c269868e9fa8fc79d55201f01d0c55b
u_m26tc <osolovyev@alfabank.ru>
2025-04-17 11:34:03 +0300

    commit

--- src/utils/events.ts
+++ src/utils/events.ts
@@ -7,7 +7,6 @@ declare global {
 
 type Payload = {
   subscription: 'month' | 'year';
-  carousel: string;
 };
 
 export const sendDataToGA = async (payload: Payload) => {
@@ -17,17 +16,17 @@ export const sendDataToGA = async (payload: Payload) => {
       now.getMonth() + 1
     }-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
 
-    await fetch(
-      'https://script.google.com/macros/s/AKfycbwKH_A70SmgSFyacS9XFhED1YEu72tg4xvMDFKJJph1hhx-RjzLfxDGBkFsO287FOJHCA/exec',
-      {
-        redirect: 'follow',
-        method: 'POST',
-        body: JSON.stringify({ date, ...payload, variant: '1' }),
-        headers: {
-          'Content-Type': 'text/plain;charset=utf-8',
-        },
-      },
-    );
+      await fetch(
+          'https://script.google.com/macros/s/AKfycbw6o0CYq_qIyuJRMDovhn6M7qJBw7FvVq8zjEIYTdhznS-p97gNeqw2Y3AwAdQAqqoGXw/exec',
+          {
+              redirect: 'follow',
+              method: 'POST',
+              body: JSON.stringify({ date, ...payload, variant: '3' }),
+              headers: {
+                  'Content-Type': 'text/plain;charset=utf-8',
+              },
+          },
+      );
   } catch (error) {
     console.error('Error!', error);
   }
